4186717fd49715468ac9e1e311c329da27720e4b
Unknown <mitch.ohara-wild@monash.edu>
2018-01-11 17:46:09 +1100

    Added google form text formatting helper

--- helpers.R
+++ helpers.R
@@ -1,3 +1,11 @@
 has_internet <- function(){
   !is.null(curl::nslookup("r-project.org", error = FALSE))
 }
+
+formText <- function(...){
+  paste(...) %>%
+    strsplit("\n", fixed=TRUE) %>%
+    unlist %>%
+    map(~ p(.)) %>%
+    tagList
+}
