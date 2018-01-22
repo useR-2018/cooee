2182d02ef6e5c997ed7e6ada5a0d756bada968e2
Unknown <mitch.ohara-wild@monash.edu>
2018-01-23 09:29:17 +1100

    App will now ask the user to install necessary packages

--- app.R
+++ app.R
@@ -1,2 +1,9 @@
+required_packages <- c("shiny", "tidyverse", "googlesheets", "DT", "jsonlite", "purrr")
+missing_packages <- !(required_packages %in% rownames(installed.packages()))
+if(any(missing_packages)){
+  stop(paste0('Some additional packages are necessary to run this app. Please install them using this command:
+       install.packages(c("', paste0(required_packages[missing_packages], collapse = '", "'), '"))'))
+}
+
 library(shiny)
 runApp(port = 4642)
