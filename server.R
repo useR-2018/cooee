259df259e99a1ab7eb3607eb3709232da951449c
Unknown <mitch.ohara-wild@monash.edu>
2018-02-08 12:50:06 +1100

    Fixed type of status in admin mode

--- server.R
+++ server.R
@@ -58,10 +58,12 @@ shinyServer(
                             else
                               tibble(reviewer, accept) %>%
                               pull(accept) %>% 
-                              {if(length(.) == 0) "None" else round(mean(recode(., "Bloody ripper" = 2, "Beaut" = 1, "Okey-dokey" = 0, "Sorry" = -1)), 2)}}
+                              {if(length(.) == 0) "None" else round(mean(recode(., "Bloody ripper" = 2, "Beaut" = 1, "Okey-dokey" = 0, "Sorry" = -1)), 2)} %>%
+                              as.numeric
+                            }
         ) %>%
         full_join(v$data, by = "id") %>%
-        replace_na(list(Reviews = 0, Status = "None", Rejects = 0)) %>%
+        replace_na(list(Reviews = 0, Status = ifelse(input$admin_mode == "Reviewer", "None", 0), Rejects = 0)) %>%
         mutate(similarity = fuzzyMatching(input$text_match, .)) %>%
         arrange(desc(similarity), Reviews)
       
